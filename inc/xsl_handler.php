ed82daa777ee3766a590b365a616ec2e93270dcf
magsilva <magsilva@3a55e87e-9307-0410-bc46-cca067d4d7cc>
2006-09-16 19:52:23 +0000

    - Implemented a small hack so the XSLT transformation could work with PHP 4 and 5. Hey PHP, why you just drop out a whole plugin, without giving a compabitility layer? You guys are very unreliable...

--- inc/xsl_handler.php
+++ inc/xsl_handler.php
@@ -1,4 +1,5 @@
 <?php
+
 function do_transformation($xml, $xsl, &$result, $option)
 // If option is set to xml, the function will return into result xml
 // If option is set to xsl, the function will return the result
@@ -12,15 +13,36 @@ function do_transformation($xml, $xsl, &$result, $option)
 	}
 	else if ($option == 'xsl')
 	{
-		$xsl_proc = xslt_create();
-		xslt_set_encoding($xsl_proc, "ISO-8859-1");
-		$arg = array('/_xml'=>$xml,);
-		$result = xslt_process($xsl_proc, 'arg:/_xml', $xsl, NULL, $arg);
-		xslt_free($xsl_proc);
-		return 1;
-	}
-	else
-	{
+
+		/**
+		* Compatibility function wrappers. These are function's wrappers that make
+		* available functions found on newer PHP versions (mostly 4.3 ones).
+		*/
+		$version = explode( ".", phpversion() );
+		$major = $version[0];
+		$minor = $version[1];
+		$release = $version[2];
+
+		if ( $major >= 5 ) {
+			$xmlDOM = new DomDocument();
+			$xslDOM = new DomDocument();
+			$xsltEngine = new XsltProcessor();
+
+			$xmlDOM->loadXML( $xml );
+			$xslDOM->load( $xsl );
+			$xsltEngine->importStylesheet( $xslDOM );
+			$transformation = $xsltEngine->transformToXml($xmlDOM);
+			echo $transformation;
+			return 1; 
+		} else {
+			$xsl_proc = xslt_create();
+			xslt_set_encoding($xsl_proc, "ISO-8859-1");
+			$arg = array('/_xml'=>$xml,);
+			$result = xslt_process($xsl_proc, $xml, $xsl, NULL, $arg);
+			xslt_free($xsl_proc);
+			return 1;
+		}
+	} else {
 		return 0;
 	}
 }
